7bbc5809c0a0dc1b87f04cfc0f719ab2b79e0725
unknown <Suleyman.Vardar@SULEYMANVRDR15Z.sahibinden.com.local>
2014-04-10 16:09:31 +0300

    lib convert to tabs

--- lib.js
+++ lib.js
@@ -2,20 +2,20 @@ var mustacheLib = mustacheLib || {};
 
 mustacheLib.prototype = {
 	renderAppend : function (templateid, values, target, callback){
-        if (typeof Mustache !== "undefined" && Mustache) {
-            var testervalue = $("#" + templateid + "").html();
-            $(target).append(Mustache.render(testervalue, values));
-            if (typeof callback !== "undefined" && callback)
-                callback.call();
-        }
-    },
+		if (typeof Mustache !== "undefined" && Mustache) {
+			var testervalue = $("#" + templateid + "").html();
+			$(target).append(Mustache.render(testervalue, values));
+			if (typeof callback !== "undefined" && callback)
+				callback.call();
+		}
+	},
 
-    renderSet : function (templateid, values, target, callback){
-        if (typeof Mustache !== "undefined" && Mustache) {
-            var testervalue = $("#" + templateid + "").html();
-            $(target).html(Mustache.render(testervalue, values));
-            if (typeof callback !== "undefined" && callback)
-                callback.call();
-        }
-    }
+	renderSet : function (templateid, values, target, callback){
+		if (typeof Mustache !== "undefined" && Mustache) {
+			var testervalue = $("#" + templateid + "").html();
+			$(target).html(Mustache.render(testervalue, values));
+			if (typeof callback !== "undefined" && callback)
+				callback.call();
+		}
+	}
 };
